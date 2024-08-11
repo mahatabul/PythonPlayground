78c1927dee6173ca24de744f12aaf4434851b2ca
Mahatabul Islam <149324015+mahatabul@users.noreply.github.com>
2024-08-11 18:46:05 +0600

    Add files via upload

--- guessthenumberUSER.py
+++ guessthenumberUSER.py
@@ -1,25 +1,26 @@
 import random
 
+
 def computerguess(x):
     low = 1
     high = x
     comment = ''
-    while comment!="c":
-        guess = random.randint(low, high)
+    while comment != "c":
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+
         comment = input(f"my guess {guess} is too high (H) or low (L) or correct (C) ?? =").lower()
-        if comment=="l":
-            low+=1
+        if comment == "l":
+            low += 1
         elif comment == "h":
-            high-=1
+            high -= 1
         else:
             print("You guessed it correct !! The number is =", guess)
             break
 
 
-
 print(" Hello User \n Give me a range and i will guess the number. \n Now Input a range.")
 range = int(input(" range = "))
 computerguess(range)
-
-
-
